a4378be9b3671660a26f4bdb78f30e627a4a7066
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-01 20:56:55 +0530

    Added Comments

--- src/Positive_Or_Negative.java
+++ src/Positive_Or_Negative.java
@@ -1,10 +1,10 @@
-import java.sql.SQLOutput;
+//This Program will find that a given number is positive or negative
 import java.util.Scanner;
 public class Positive_Or_Negative {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         System.out.println("Enter Number");
-        int n= in.nextInt();
+        int n= in.nextInt();//Input number
         if (n>0){
             System.out.println("Number is positive");
         }
