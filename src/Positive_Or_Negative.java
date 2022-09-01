2db5e21e22efe74d12a6d676b5204be77498b357
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-01 20:47:06 +0530

    Some Changes

--- src/Positive_Or_Negative.java
+++ src/Positive_Or_Negative.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Positive_Or_Negative {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Positive_Or_Negative {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n= in.nextInt();
+        if (n>0){
+            System.out.println("Number is positive");
+        }
+        else if(n<0) {
+            System.out.println("Number is Negative");
+        }
+        else {
+            System.out.println("Number is Zero");
+        }
+
+    }
 }
